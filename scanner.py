dfd06781d20356a9868ee8f957f7b1259b1cdf06
skup111 <167567970+skup111@users.noreply.github.com>
2024-04-23 14:42:05 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,4 +1,12 @@
 import socket
+import threading
+
+def scan_port(target, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print("Port: " + str(port) + " Open")
+    s.close()
 
 print("Please enter an IP Address to scan.")
 target = input("> ")
@@ -7,9 +15,15 @@ print("*" * 40)
 print("* Scanning: " + target + " *")
 print("*" * 40)
 
+threads = []
+
 for port in range(1, 1025):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = s.connect_ex((target, port))
-    if result == 0:
-        print("Port: " + str(port) + " Open")
-    s.close()
+    t = threading.Thread(target=scan_port, args=(target, port))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print("Scanning is completed")
+
